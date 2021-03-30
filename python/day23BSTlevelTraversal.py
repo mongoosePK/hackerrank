a4f93500b72a4f837360b28660be6d431a65b29d
mongoosePK <30559692+mongoosePK@users.noreply.github.com>
2021-03-29 19:02:34 -0500

    Add binary serach tree problems in python

---
+++ python/day23BSTlevelTraversal.py
@@ -0,0 +1,36 @@
+# day23BinarySearchTrees.py
+# william pulkownik
+# depth traversal of a BST
+
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        nodeQueue = [root]
+        for node in filter(None, nodeQueue):
+            print(node.data, end=' ')
+            nodeQueue += [node.left, node.right]
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
